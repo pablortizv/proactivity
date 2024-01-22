0ec1c0d4d88bff42ca34d51105bee3b84c8e590f
Pablortizv <pablortizv@gmail.com>
2024-01-22 11:30:54 -0600

    change data

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -3,16 +3,19 @@ import { getFirestore } from "firebase/firestore";
 
 // Configuración de firebase se guarda en .env para evitar mal uso
 const firebaseConfig = {
-  apiKey: "AIzaSyBbcqkSR6vCRuFKPmfC_80-7vLTsXHK94Y",
-  authDomain: "proactivity-db.firebaseapp.com",
-  projectId: "proactivity-db",
-  storageBucket: "proactivity-db.appspot.com",
-  messagingSenderId: "74758377432",
-  appId: "1:74758377432:web:9f0c90aae2faa6e967acad",
-  measurementId: "G-XBW5BTYEMS"
+  apiKey: process.env.NEXT_PUBLIC_FIRESTORE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIRESTORE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIRESTORE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIRESTORE_MEASUREMENT_ID
   };
   
 // Inicializamos Firebase
 initializeApp(firebaseConfig);
 
 export const db = getFirestore();
+
+
+
